3995c2e64c9c32168a83c02d6b14424ea3ed69bf
Maor <maorfeldinger@gmail.com>
2017-11-13 23:47:08 -0800

    remove bootstrap cdn

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 
 //Middlware
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
