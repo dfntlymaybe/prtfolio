c6aa6104228b9c6e2939ef06a91b209667b3bb8d
Maor <maorfeldinger@gmail.com>
2017-11-13 19:10:04 -0800

    add config file

---
+++ config.js
@@ -0,0 +1,13 @@
+
+var config = {};
+
+config.sender = {
+  user: 'sholehmailim@gmail.com',
+  pass: 'zxcvb09876'
+}
+
+config.receiver = {
+  user: 'rounder236@gmail.com'
+}
+
+ module.exports = config;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var nodeMailer = require('nodemailer');
+var config = require('./config');
 
 //Set up
 var app = express();
@@ -12,36 +13,16 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
-  
 //Set up nodeMailer with my gmail cradentials
 var transporter = nodeMailer.createTransport({
   service: "Gmail",
     auth:{
-      user: 'sholehmailim@gmail.com',
-      pass: 'zxcvb09876'
+      user: config.sender.user,
+      pass: config.sender.pass
 
     }
-      // auth: {
-      //   type: 'OAuth2',
-      //   user: 'mailim@gmail.com',
-      //   clientId: '1072681813233-k2o7dvuobe104el1dt0fksmqbcnaul66.apps.googleusercontent.com',
-      //   clientSecret: 'MO4PCf2NTRsC23_dCAafRSg-',
-      //   access_token:"ya29.GlsCBWnC_RjIiOS6leBq86TNvUq2hPHOH6Cti0-iqLa6mcp_KPJhkJE2f-h9d67PZKgATW-bJUAaDtMF3Dod9pXUfc5jNR1WEa_uGSYilMG5LvbqwoI20KRZ0ctC",
-      //   refresh_token:"1/NLf6mAbiV_x_pirnm0ExYp5o6dPWaBG8V_B6vQNafx4",
-      //   expiry_date:1510529637064
-      // }
-});
 
-// transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
-//     var accessToken = userTokens[user];
-//     if(!accessToken){
-//         return callback(new Error('Unknown user'));
-//     }else{
-//         return callback(null, accessToken);
-//     }
-// });
-//Handlers
+});
 
 //Sending HTML on first GET
 app.get('/', function (req, res) {
@@ -57,8 +38,8 @@ app.post('/mail', function(req, res){
   '\nmessage: ' + req.body.text;
 
   var mailOptions = {
-      from: 'sholehmailim@gmail.com', // sender address
-      to: 'rounder236@gmail.com', // list of receivers
+      from: config.sender.user, // sender address
+      to: config.receiver.user, // list of receivers
       subject: 'New portfolio web message: ' + req.body.subject, // Subject line
       text: message //, // plaintext body
   };
