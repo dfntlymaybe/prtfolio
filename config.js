dedb763a703822da88bce7d03833fe25d0025549
Maor <maorfeldinger@gmail.com>
2018-01-11 10:20:13 -0800

    add to port to config

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ config.sender = {
   user: 'sholehmailim@gmail.com',
   pass: 'zxcvb09876'
 }
-
+ config.port = "5000";
+ 
 config.receiver = {
   user: 'rounder236@gmail.com'
 }
--- server.js
+++ server.js
@@ -56,5 +56,5 @@ app.post('/mail', function(req, res){
   });
 });
 
-app.listen(process.env.PORT || '5000');
+app.listen(process.env.PORT || config.port);
 console.log("Its all starts here...");
